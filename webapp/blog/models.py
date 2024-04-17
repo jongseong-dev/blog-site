1a383b9d2a9b7e41f66f01560d050d66f8352324
jongseong-keep-going-dev <60998945+jongseong-dev@users.noreply.github.com>
2024-04-17 23:05:34 +0900

    Blog 모델 객체 생성 (#3)
    * chore: black 추가
    
    * feat: pyrightconfig.json 추가
    
    * feat(blog.models): post 모델 객체 정의
    
    * feat(migration): blog model init migration
    
    * style(base): 포매팅 수정
    
    ---------
    
    Co-authored-by: jslee <dlwhdtjd098@naver.com>

--- webapp/blog/models.py
+++ webapp/blog/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class DefaultModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Post(DefaultModel):
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)  # 문자, 숫자, 밑줄 또는 하이픈만 포함하는 짧은 레이블
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        # 역방향 관계를 지칭함 User 객체에서 post 속성을 이용할 수 있음
+        # ex) user.blog_posts
+        related_name="blog_posts",
+    )
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
+
+    def __str__(self):
+        return self.title
