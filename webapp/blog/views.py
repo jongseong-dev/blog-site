46feefeee48b3eca2e22e2c7d5c3ed7bb6476251
jslee <dlwhdtjd098@naver.com>
2024-04-18 18:14:18 +0900

    feat(blog.views): post list, detail view func 추가

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,5 +1,19 @@
+from blog.models import Post
+from django.http import Http404
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
 
 def home(request):
-    return JsonResponse({'page': 'blog'})
+    return JsonResponse({"page": "blog"})
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk, status=Post.Status.PUBLISHED)
+    return render(request, "blog/post/detail.html", {"post": post})
