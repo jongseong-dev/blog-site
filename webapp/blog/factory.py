f57509b121fdf72221e43fb9a0d25689c6f7038f
jslee <dlwhdtjd098@naver.com>
2024-04-18 20:33:28 +0900

    feat(factory): Post 테스트를 위한 Factory 추가

---
+++ webapp/blog/factory.py
@@ -0,0 +1,23 @@
+import factory
+from blog.models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f"User {n}")
+
+
+class PostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Post
+        django_get_or_create = ("title",)
+
+    title = factory.Sequence(lambda n: f"Post {n}")
+    publish = factory.LazyFunction(timezone.now)
+    author = factory.SubFactory(UserFactory)
+    body = factory.Sequence(lambda n: f"Body {n}")
+    status = factory.LazyAttribute(lambda o: Post.Status.PUBLISHED)
