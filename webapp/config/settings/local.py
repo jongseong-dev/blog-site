fb21becf69801e353afd15ef49868215a57e49f3
jslee <dlwhdtjd098@naver.com>
2024-04-21 00:54:22 +0900

    chore(settings.local): debug_toolbar 가 docker 환경에서 구동 되도록 수정

--- webapp/config/settings/local.py
+++ webapp/config/settings/local.py
@@ -16,6 +16,12 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+# docker 에서 실행 시 internal ips가 동적으로 할당 되므로, 아래와 같이 설정해준다
+import socket
+
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
