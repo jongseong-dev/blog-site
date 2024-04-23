b6814cb9b1f2f947b4077f6499109e61f14321d3
jslee <dlwhdtjd098@naver.com>
2024-04-23 19:48:06 +0900

    feat(blog.views): 페이지 오류 처리

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,4 +1,6 @@
 from blog.models import Post
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.http import JsonResponse
@@ -10,7 +12,14 @@ def post_list(request):
     posts = Post.published.all()
     paginator = Paginator(posts, 3)
     page_number = request.GET.get("page", 1)
-    posts_with_pagination = paginator.page(page_number)
+    try:
+        posts_with_pagination = paginator.page(page_number)
+    except PageNotAnInteger:
+        # 페이지 번호가 정수가 아닌 경우 결과의 첫 페이지를 전달
+        posts_with_pagination = paginator.page(1)
+    except EmptyPage:
+        # 페이지 번호가 범위를 벗어난 경우 결과의 마지막 페이지를 전달
+        posts_with_pagination = paginator.page(paginator.num_pages)
     return render(
         request, "blog/post/list.html", {"posts": posts_with_pagination}
     )
