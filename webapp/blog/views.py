970f56013f640e4790a7785da53823911966f90b
jslee <dlwhdtjd098@naver.com>
2024-04-23 19:40:19 +0900

    feat: 페이지네이션 적용하기

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,4 +1,5 @@
 from blog.models import Post
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
@@ -7,7 +8,12 @@ from django.shortcuts import render
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts": posts})
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get("page", 1)
+    posts_with_pagination = paginator.page(page_number)
+    return render(
+        request, "blog/post/list.html", {"posts": posts_with_pagination}
+    )
 
 
 def post_detail(request, year: int, month: int, day: int, post: Post):
