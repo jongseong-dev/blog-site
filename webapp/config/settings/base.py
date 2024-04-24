a121066590877af7da2a755a1e619abc02e803ed
jongseong-keep-going-dev <60998945+jongseong-dev@users.noreply.github.com>
2024-04-24 23:51:35 +0900

    github actions 에서 Docker Image로 CI 하기 (#13)
    * modify: precommit에서 poetry export 를 삭제하고, github actions 에서 export 하도록 코드 수정
    
    * modify: pr template 수정
    
    * fix: poetry plugin 오류 수정
    
    * modify: lint step 을 위한 flake8 설치
    
    * rename: lint와 test를 확인하는 것이 명확하도록 파일이름 변경
    
    * feat: docker image ci
    
    * fix: dockerhub token 으로 secrets 변수명 수정
    
    * fix: docker image tag semVer 버전 네이밍 수정
    
    * modify(config): base config 의 secret key 삭제
    
    * docs: README.md 수정
    
    * modify: semVer patch를  github build number 로 수정
    
    * modify: semVer Version 수정
    
    1. issue number 를 major
    2. 오늘 연월일을 minor로
    3. build number 는 patch 로 수정
    
    * fix: env 위치 수정
    
    * fix: env 삭제
    
    * fix: issue number 수정
    
    * modify: 환경에 따른 배포를 env 변수를 통해 하기 위해 docker 수정
    
    * modify: build-push-action에 target 변수 추가
    
    * feat: Dockerfile expose 추가
    
    * modify: build-push-action에 target 변수 삭제 및 버전 수정
    
    * modify: image push test 를 위한 event 삭제
    
    * modify: github semVer version 수정
    
    ---------
    
    Co-authored-by: jslee <dlwhdtjd098@naver.com>

--- webapp/config/settings/base.py
+++ webapp/config/settings/base.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = []
 
