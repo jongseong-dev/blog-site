0708106bee59fefa5378b876c0bd5fc7a5686025
jslee <dlwhdtjd098@gmail.com>
2024-04-25 14:50:43 +0900

    feat(blog.models): 댓글 model 추가

--- webapp/blog/models.py
+++ webapp/blog/models.py
@@ -67,3 +67,22 @@ class Post(DefaultFieldModel):
                 self.slug,
             ],
         )
+
+
+class Comment(DefaultFieldModel):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["created_at"]
+        indexes = [
+            models.Index(fields=["created_at"]),
+        ]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
