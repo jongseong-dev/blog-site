b6e856bae5f9a7fbea5553355330c5136d5f1ccb
jongseong-keep-going-dev <60998945+jongseong-dev@users.noreply.github.com>
2024-04-28 19:47:52 +0900

    Markdown을 html로 랜더링하는 기능 추가 (#18)
    * feat: 마크다운을 html로 랜더링하는 기능 추가
    
    * modify: setUpClass -> setUpTestData 로 method 변경

--- webapp/blog/templatetags/blog_tags.py
+++ webapp/blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+from markdown import markdown
 
 from ..models import Post
 
@@ -22,3 +24,9 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count("comments")).order_by(
         "-total_comments"
     )[:count]
+
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    # mark_safe: 문자열을 HTML로 렌더링할 때 사용. 이스케이프 처리를 하지않음
+    return mark_safe(markdown(text))
--- webapp/blog/tests/test_templatetags.py
+++ webapp/blog/tests/test_templatetags.py
@@ -1,11 +1,13 @@
 from django.template import Template, Context
 from django.test import TestCase
+from django.utils.safestring import SafeString
 
 from blog.factory import PostFactory, CommentFactory
 from blog.templatetags.blog_tags import (
     get_most_commented_posts,
     total_posts,
     show_latest_posts,
+    markdown_format,
 )
 
 
@@ -39,3 +41,37 @@ class PostTests(TestCase):
     def test_get_most_commented_posts(self):
         result = get_most_commented_posts(5)
         self.assertEqual(result.count(), 5)
+
+    def test_rendered_markdown_filter(self):
+        markdown_text = (
+            "# Heading\n\nThis is a paragraph "
+            "with *italic* and **bold** text."
+        )
+        expected_html = (
+            "<h1>Heading</h1>\n<p>This is a paragraph "
+            "with <em>italic</em> "
+            "and <strong>bold</strong> text.</p>"
+        )
+
+        template_str = "{% load blog_tags %}{{ markdown_text|markdown }}"
+        template = Template(template_str)
+        context = Context({"markdown_text": markdown_text})
+        rendered_template = template.render(context)
+
+        self.assertIsInstance(rendered_template, SafeString)
+        self.assertEqual(rendered_template, expected_html)
+
+    def test_get_markdown_filter(self):
+        markdown_text = (
+            "# Heading\n\nThis is a paragraph "
+            "with *italic* and **bold** text."
+        )
+        expected_html = (
+            "<h1>Heading</h1>\n<p>This is a paragraph "
+            "with <em>italic</em> "
+            "and <strong>bold</strong> text.</p>"
+        )
+
+        result = markdown_format(markdown_text)
+        self.assertIsInstance(result, SafeString)
+        self.assertEqual(result, expected_html)
