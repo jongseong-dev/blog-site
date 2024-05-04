2ce58a7649453c0760a4544a93a52c2127a93b58
jslee <dlwhdtjd098@gmail.com>
2024-05-04 16:18:35 +0900

    feat(blog.views): 검색을 위한 views 작성

--- webapp/blog/views.py
+++ webapp/blog/views.py
@@ -1,4 +1,6 @@
-from blog.forms import CommentForm
+from django.contrib.postgres.search import SearchVector
+
+from blog.forms import CommentForm, SearchForm
 from blog.forms import EmailPostForm
 from blog.models import Post
 from django.conf import settings
@@ -117,4 +119,23 @@ def post_comment(request, post_id):
         request,
         "blog/post/comment.html",
         {"post": post, "form": form, "comment": comment},
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
     )
